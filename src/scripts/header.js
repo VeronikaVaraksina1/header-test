03e4373cc9c6dd9ae166ab2fe342fe626b7e8b20
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-01-27 21:09:20 +0200

    test

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -26,8 +26,9 @@ function changeActiveLink() {
 
   window.addEventListener('load', () => {
     const currentPage = window.location.pathname;
+    console.log(currentPage);
 
-    if (currentPage.includes('index.html') || currentPage == '/header-test/') {
+    if (currentPage.includes('index.html') || currentPage === '/header-test/') {
       links[0].classList.add('active-link');
       links[2].classList.add('active-link');
     } else if (currentPage.includes('page-2.html')) {
