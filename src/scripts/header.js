646086ec301b44c9c85638671825ede74452b4ff
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-01-27 16:41:28 +0200

    change a function

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -26,25 +26,44 @@ function changeActiveLink() {
   const homeLink = document.querySelector('.home');
 
   window.addEventListener('load', () => {
-    const currentPage = window.location.href;
+    const currentPage = window.location.pathname;
 
     links.forEach(link => {
-      console.log(currentPage);
-      console.log(window.location.href);
-      console.log(
-        currentPage === 'https://veronikavaraksina1.github.io/header-test/'
-      );
-      console.log(link.getAttribute('href') === currentPage);
-
-      if (currentPage === 'https://veronikavaraksina1.github.io/header-test/') {
+      if (currentPage === '/header-test/') {
+        // на живій сторінці працює варіант currentPage === '/header-test/'
         homeLink.classList.add('active-link');
       } else {
         if (link.getAttribute('href') === currentPage) {
           link.classList.add('active-link');
         }
       }
+
+      // console.log test
+      console.log(currentPage);
+      console.log(window.location.pathname);
+      console.log(currentPage === '/header-test/');
+      console.log(link.getAttribute('href') === currentPage);
     });
   });
 }
 
 changeActiveLink();
+
+// function changeActiveLink() {
+//   const links = document.querySelectorAll('.nav-link');
+//   const homeLink = document.querySelector('.home');
+
+//   window.addEventListener('load', () => {
+//     const currentPage = window.location.href;
+
+//     links.forEach(link => {
+//       if (currentPage === 'https://veronikavaraksina1.github.io/header-test/') {
+//         homeLink.classList.add('active-link');
+//       } else {
+//         if (link.getAttribute('href') === currentPage) {
+//           link.classList.add('active-link');
+//         }
+//       }
+//     });
+//   });
+// }
